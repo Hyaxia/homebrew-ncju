de0f313b30ac35537a77b6633653fa8b3bbc43c8
hyaxia <hyaxia@gmail.com>
2025-03-23 18:19:02 +0200

    updated version

--- Formula/ncju.rb
+++ Formula/ncju.rb
@@ -3,8 +3,8 @@ class Ncju < Formula
 
   desc "A CLI tool for viewing and navigating JSON files"
   homepage "https://github.com/Hyaxia/ncju"
-  url "https://github.com/Hyaxia/ncju/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "1d456caed0c1018fdbf8129aceac49ef0193ef48ed2caf27e24849e477d58339"
+  url "https://github.com/Hyaxia/ncju/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "0bc6a5bb6fa5e5c36f2021463f0aeeeee5deb29cb1e7ad06c2d8fba9f91bc4cc"
 
   depends_on "python@3.12"
 
